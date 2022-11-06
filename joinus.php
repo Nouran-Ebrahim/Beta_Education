eea52b62f924360b70140d05f64c51540d3e60fd
unknown <alaaosama2121998@gmail.com>
2022-11-06 22:26:38 +0200

    form

--- joinus.php
+++ joinus.php
@@ -5,74 +5,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/form.css">
+    <link rel="stylesheet" href="assets/css/reg.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Join Us</title>
 </head>
 
 <body>
-    <div class="container right-panel-active">
-        <!-- Sign Up -->
-        <div class="container__form container--signup">
-            <form action="#" class="form" id="form1">
-                <h2 class="form__title">Sign Up</h2>
-                <input type="text" placeholder="User Name" class="input" />
-                <input type="email" placeholder="Email" class="input" />
-                <!-- 
-			<input type="radio" placeholder="" class="input" value="Female" />
-			<lable>Female</lable>
-			
-			<input type="radio" placeholder="" class="input" value="Male" />
-		<lable>Male</lable> -->
 
-                <input type="text" placeholder="City" class="input" />
-                <input type="text" placeholder="School" class="input" />
-                <div class="grad">
-                    <label for="cars">Your Grad:</label>
-                    <select name="Grad" id="Grade">
-                        <option value="1st prep">1st Preparatory</option>
-                        <option value="2nd prep">2nd Preparatory</option>
-                        <option value="3rd prep">3rd Preparatory</option>
-                        <option value="1st prep">1st Secondary</option>
-                        <option value="2nd prep">2nd Secondary</option>
-                        <option value="3rd prep">3rd Secondary</option>
-
-                    </select>
-                </div>
-                <div class="grad">
-                    <input type="radio" class="input" value="English" />
-                    <lable>English</lable>
-                </div>
-                <div class="grad">
-                    <input type="radio" class="input" value="Arabic" />
-                    <lable>Arabic</lable>
-                </div>
-                <button class="btn">Sign Up</button>
-            </form>
-        </div>
-
-        <!-- Sign In -->
-        <div class="container__form container--signin">
-            <form action="#" class="form" id="form2">
-                <h2 class="form__title">Sign In</h2>
-                <input type="email" placeholder="Email" class="input" />
-                <input type="password" placeholder="Student code" class="input" />
-                <button class="btn">Sign In</button>
-            </form>
-        </div>
-
-        <!-- Overlay -->
-        <div class="container__overlay">
-            <div class="overlay">
-                <div class="overlay__panel overlay--left">
-                    <button class="btn" id="signIn">Sign In</button>
-                </div>
-                <div class="overlay__panel overlay--right">
-                    <button class="btn" id="signUp">Sign Up</button>
+    <!-- Sign Up -->
+	<div class="reg">
+    <form >
+        <div class="form-col">
+            <div class="form-group w-100">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+            <div class="form-col">
+                <div class="form-group ">
+                    <label for="inputEmail4">Name</label>
+                    <input type="text" class="form-control" id="inputEmail4" placeholder="Name">
                 </div>
             </div>
-        </div>
-    </div>
-    <script src="assets/js/form.js"></script>
+            <div class="form-group">
+			<label for="inputPassword4">Student Code</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Student Code">
+            </div>
+			<br>
+			<div class="center">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+			</div>
+			<div class="form-group">
+                <div class="center">
+					<br>
+				<a href="register.php">Create New Accounte</a>
+                </div>
+            </div>
+    </form>
+</div>
+
+    <script src="assets/js/reg.js"></script>
 
 </body>
 
